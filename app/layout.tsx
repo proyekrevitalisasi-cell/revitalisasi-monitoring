21a66df11a24dc3bc08ca87f635ace48e5082391
salomositorus <52040135+salomositorus@users.noreply.github.com>
2026-07-01 11:19:42 +0700

    fix: reinitialize shadcn/ui with correct style (default) and base color (slate)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="font-sans">
+    <html lang="en" className={cn("font-sans")}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
